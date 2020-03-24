176a0ca7dc71fb7d096958956dd4fbfbcffa6940
liampmoore <16942895+liampmoore@users.noreply.github.com>
2020-03-24 13:04:33 -0400

    Implemented auth router

---
+++ auth/restricted-middleware.js
@@ -0,0 +1,23 @@
+const jwt = require("jsonwebtoken")
+
+module.exports = (req, res, next) => {
+  const { authorization } = req.headers;
+
+  const secret = require("../config/secrets").jwtSecret;
+
+
+  if (authorization) {
+    jwt.verify(authorization, secret, (err, decodedToken) => {
+      if (err) {
+      res.status(401).json({ message: 'Invalid Credentials' });
+      }
+      else {
+        req.decodedToken = decodedToken;
+        next();
+      }
+    })
+  }
+  else {
+    res.status(400).json({message: "NO credentials provided"})
+  }
+};
